630e75a3118c1cfc82204a5f2437a9ee2f4d23f9
thanhtamcntt12 <thanhtamcntt12@gmail.com>
2018-06-19 10:09:27 +0700

    #2 commit

--- pluton/src/app/app.module.ts
+++ pluton/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PriceComponent } from './price/price.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'services', component: ServicesComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'clients', component: ClientsComponent },
+  { path: 'price', component: PriceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    FooterComponent,
+    FooterComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ClientsComponent,
+    PriceComponent,
+    ContactComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
+  imports:[ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
